bd7f15b0a16e5873896c2c720836cb1b65174454
kounou25 <kounougilbert288@gmail.com>
2025-02-06 10:25:35 +0100

    mise en place d'une animation de lors du loading des pages

--- src/app/employees/page.js
+++ src/app/employees/page.js
@@ -52,11 +52,11 @@ export default function EmployeeDashboard() {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center h-screen bg-gray-900">
+      <div className="flex items-center justify-center h-screen bg-transparent">
         <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
       </div>
     );
-  }
+}
 
   return (
     <div className="flex h-screen bg-gray-100">
