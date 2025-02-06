9aa6fe2aabfc24f88bfcb4de7b68646529348dfb
kounou25 <kounougilbert288@gmail.com>
2025-02-06 12:47:19 +0100

    everything is okay

--- src/app/components/empSidebar.js
+++ src/app/components/empSidebar.js
@@ -11,7 +11,7 @@ const navigationItems = [
   { name: 'Plannings', icon: Calendar, path: '/employees/plannings' },
   { name: 'Tâches', icon: CircleCheckBig, path: '/employees/tasks' },
   { name: 'Utilisateurs', icon: Users, path: '/employees/users' },
-  { name: 'Permissions', icon: Shield, path: '/employees/permissions' },
+  { name: 'Permissions', icon: Shield, path: '/employees/permission' },
   { name: 'Rapports', icon: ClipboardMinus, path: '/employees/rapport' },
   { name: 'Paramètres', icon: Settings, path: '/settings' },
 ];
@@ -27,7 +27,7 @@ export default function EmpSidebar() {
       className="w-72 bg-gray-900 text-white shadow-lg p-6 flex flex-col"
     >
       <h1 className="text-2xl font-bold flex items-center gap-2">
-        <Calendar className="text-blue-500" /> ECOLO2LODGY
+        <Calendar className="text-blue-500" /> ECO2LODGY
       </h1>
       <nav className="mt-8 space-y-4">
         {navigationItems.map((item) => (
