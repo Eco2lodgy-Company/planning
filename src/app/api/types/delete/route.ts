4f0d112845e01d04cb6ba34a28686b217a909ec2
asaleydiori <asaleydiori@gmail.com>
2025-02-26 09:39:50 +0100

    editer et supprimer des types de projets

--- src/app/api/types/delete/route.ts
+++ src/app/api/types/delete/route.ts
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server";
 export async function DELETE(req:Request) {
   try {
     const body = await req.json();
-    const { id } = body;
+    const { id_type } = body;
 
-    if (!id) {
+    if (!id_type) {
       return NextResponse.json({ error: "Le champ 'id' est obligatoire." }, { status: 400 });
     }
 
-    const { data, error } = await supabase.from("typeprojet").delete().eq("id", id);
+    const { data, error } = await supabase.from("typeprojet").delete().eq("id_type", id_type);
 
     if (error) {
       return NextResponse.json({ error: error.message }, { status: 400 });
