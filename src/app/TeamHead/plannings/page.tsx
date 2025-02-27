7b19a9f54f59ab3d25b31a1e3156dd77df907e8c
kounou25 <kounougilbert288@gmail.com>
2025-02-27 18:05:04 +0100

    nothing to commit, everything is working 😂

---
+++ src/app/TeamHead/plannings/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { useEffect, useState } from "react";
+import Calendrier from "./Calendrier";
+
+const Home = () => {
+  const [taches, setTaches] = useState([]);
+  const [utilisateurs, setUtilisateurs] = useState([]);
+
+  useEffect(() => {
+    // Simuler une requête API pour récupérer les tâches
+    fetch('/api/tache')
+      .then(response => response.json())
+      .then(data => setTaches(data.data));
+
+    // Simuler une requête API pour récupérer les utilisateurs
+    fetch('/api/users')
+      .then(response => response.json())
+      .then(data => setUtilisateurs(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Calendrier des Tâches</h1>
+      <Calendrier taches={taches} utilisateurs={utilisateurs} />
+    </div>
+  );
+};
+
+export default Home;
