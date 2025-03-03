17a2b22f4750bd772d6c29e6c6cf7833ee682fe1
kounou25 <kounougilbert288@gmail.com>
2025-03-03 10:26:11 +0100

    created task attribution endpoint (PUT)

---
+++ src/app/api/headside/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { supabase } from "@/lib/SupabaseClient";
+
+
+export async function PUT() {
+    try {
+        
+        const { id_user,datedebut,id_tache } = await req.json();
+
+        if (!id_user || !datedebut || !id_tache) {
+            return NextResponse.json({ error: "Données invalides" }, { status: 400 });
+        }
+
+        // Mise à jour de la permission dans Supabase
+        const { data, error } = await supabase
+            .from("taches") // Remplace par le nom de ta table
+            .update({ id_user: id_user,datedebut:datedebut })
+            .eq("id_tache", id_tache)
+            .select(); // Récupère la ligne mise à jour
+
+        if (error) {
+            throw new Error(error.message);
+        }
+
+        return NextResponse.json({ message: "permission mise à jour", task: data[0] });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: "Erreur serveur", details: error.message }, { status: 500 });
+    }
+}
