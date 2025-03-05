f0fe2d3d7741fc52be3531da7288ea1069e8e3ed
asaleydiori <asaleydiori@gmail.com>
2025-03-05 12:46:16 +0100

    ajout des taches sur la nouvelle page

--- src/app/api/dashboardAdmin/empActif/route.ts
+++ src/app/api/dashboardAdmin/empActif/route.ts
@@ -4,11 +4,22 @@ import { supabase } from "@/lib/SupabaseClient";
 export async function GET() {
   try {
     const today = new Date().toISOString().split("T")[0];
+    
+    // Use a subquery to get the first login for each user today
     const { data, error } = await supabase
       .from("logs")
       .select("user", { count: "exact" })
-      .gte("login_time", `${today}T00:00:00`)
-      .lte("login_time", `${today}T23:59:59`);
+      .eq(
+        "id", 
+        supabase
+          .from("logs")
+          .select("id")
+          .gte("login_time", `${today}T00:00:00`)
+          .lte("login_time", `${today}T23:59:59`)
+          .order("login_time", { ascending: true })
+          .group("user")
+          .select("id")
+      );
 
     if (error) throw error;
 
